5437056a9956bbe8d5d01f682e4f3434442c3d9f
Alexander Blech <alexander.blech@fu-berlin.de>
2024-07-23 08:39:29 +0200

    Add license notice to all source files

--- src/openquad/lebedev.py
+++ src/openquad/lebedev.py
@@ -1,3 +1,19 @@
+# OpenQuad - open database for multi-dimensional numerical integration
+# Copyright (C) 2024  Alexander Blech
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library. If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 
 from .base import AtomicS2Quadrature, QuadratureWithDegree
