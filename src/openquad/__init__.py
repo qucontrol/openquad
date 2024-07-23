175fa658a8019748f87b3f914b43637484a979a7
Alexander Blech <alexander.blech@fu-berlin.de>
2024-07-23 07:29:54 +0200

    Update submodule names in __init__.py

--- src/openquad/__init__.py
+++ src/openquad/__init__.py
@@ -1,2 +1,19 @@
 
 __version__ = "alpha+dev"
+
+# Expose top-level interface
+from .geometries import Rn, S2, SO3
+
+# Expose submodules
+from . import (
+    geometries,
+    newton_cotes,
+    gauss,
+    lebedev,
+    monte_carlo,
+    grid,
+    graef,
+    womersley,
+    karney,
+    fibonacci,
+)
