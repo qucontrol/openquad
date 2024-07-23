1c795e60c4ff14b6dc093070d760bd3cabd78cfb
Alexander Blech <alexander.blech@fu-berlin.de>
2024-07-23 12:24:53 +0200

    Update test_base.py

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,14 +1,12 @@
-import numpy as np
-
 import pytest
 from contextlib import nullcontext as does_not_raise
 
-from openquad.quad import TopologicalQuadrature
+from openquad.geometries import GeometryQuadrature
 
 
 def test_all_method_aliases_are_valid():
     """Test that all method aliases point to valid method names."""
-    method_aliases = TopologicalQuadrature._method_aliases
-    method_names = TopologicalQuadrature._available_methods
+    method_aliases = GeometryQuadrature._method_aliases
+    method_names = GeometryQuadrature._available_methods
     for method in method_aliases.values():
         assert method in method_names.keys()
