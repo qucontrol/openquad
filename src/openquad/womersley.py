9d9cec8306c76635ab38ea36065a3afc299b85a5
Alexander Blech <alexander.blech@fu-berlin.de>
2024-07-23 07:19:05 +0200

    Import implementation of quadratures from Womersley

---
+++ src/openquad/womersley.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from .base import (AtomicSO3Quadrature, AtomicS2Quadrature, QuadratureWithPolyAcc)
+from .data.womersley.table import S2_SPHERICAL_DESIGNS
+
+
+class WomersleyS2Design(AtomicS2Quadrature, QuadratureWithPolyAcc):
+
+    _TABLE = np.asarray(S2_SPHERICAL_DESIGNS, dtype=int)
+    _available_sizes = _TABLE[:, 1]
+    _available_p_accs = _TABLE[:, 0]
+
+    def __init__(self, *args, symmetric=None, **kwargs):
+        super().__init__(*args, symmetric=symmetric, **kwargs)
+
+    def _points_weights(self, symmetric):
+        """S2 spherical designs from Womersley.
+
+        S2 spherical designs taken from:
+        https://web.maths.unsw.edu.au/~rsw/Sphere/
+
+        Returns
+        -------
+        points : ndarray
+            Sample points.
+        weights : ndarray
+            Quadrature weights.
+
+        """
+        if symmetric is not None:
+            if symmetric:
+                suffix = '_sym'
+            else:
+                suffix = '_nosym'
+            points, weights = self._load_points_weights(
+                'womersley',
+                's2_design',
+                suffix,
+            )
+        if symmetric is None:
+            # First try to find a symmetric method. If no present, try
+            # asymmetric
+            try:
+                points, weights = self._load_points_weights(
+                    'womersley',
+                    's2_design',
+                    '_sym',
+                )
+            except FileNotFoundError:
+                points, weights = self._load_points_weights(
+                    'womersley',
+                    's2_design',
+                    '_nosym',
+                )
+        weights = weights * (4*np.pi)
+        return points, weights
+
+
+class WomersleySO3EqualWeight(AtomicSO3Quadrature, QuadratureWithPolyAcc):
+
+    _TABLE = np.asarray(S2_SPHERICAL_DESIGNS, dtype=int)
+    _available_sizes = _TABLE[:, 1] // 2
+    _available_p_accs = (_TABLE[:, 0] - 1) // 2
+    # p_acc_SO3 corresponds to p_acc_S3 = 2*p_acc_SO3 + 1
+
+    def _points_weights(self):
+        """SO3 quadrature method based on S3 spherical designs from Womersley.
+
+        S3 spherical designs taken from:
+        https://web.maths.unsw.edu.au/~rsw/Sphere/
+
+        Returns
+        -------
+        points : ndarray
+            Sample points.
+        weights : ndarray
+            Quadrature weights.
+
+        """
+        msg = "SO3 quadratures from Womersley not yet implemented"
+        raise NotImplementedError(msg)
+
+        points, weights = self._load_points_weights('womersley', 's3_design')
+        # data contains the cartesian coordinates
+        # TODO: identify opposite points
+        # TODO: convert to Euler angle representation
+        # TODO: remove NotImplementedError at beginning of this method
+        points = ...
+        weights = np.ones(self.size) / self.size * (8*np.pi**2)
+        return points, weights
